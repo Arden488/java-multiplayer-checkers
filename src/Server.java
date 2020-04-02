a411a3320cb5e35cf67e603a636ba643e8a2f56b
Anton Samoilov <absamoilov88@gmail.com>
2020-04-02 14:44:02 +0100

    Create server class

---
+++ src/Server.java
@@ -0,0 +1,67 @@
+/**
+ * Server class
+ * Author: Anton Samoilov <2459087s@student.gla.ac.uk>, matric 2459087S
+ * ------------
+ * This class creates multiple threads to handle connecting clients
+ */
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+    // Port to connect
+    private final int PORT = 5678;
+    private ServerSocket server;
+
+    /**
+     * Constructor
+     */
+    public Server() {
+        // Create server socket to wait for connections
+        try {
+            server = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Run method for the thread
+     */
+    public void run() {
+        while (true) {
+            Socket clientSocket = null;
+
+            try {
+                // Wait for connection and create a new client
+                clientSocket = server.accept();
+                System.out.println("New client connected");
+
+                // Create a client worker
+                ServerWorker client = new ServerWorker(clientSocket);
+
+                // Create a client thread
+                new Thread(client).start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Main method
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Create and start a thread for server
+        Thread thread = new Thread(new Server());
+        thread.start();
+
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
