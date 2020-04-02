8a2449e22f44372c7255eb6b31255bd8f59b4c39
Anton Samoilov <absamoilov88@gmail.com>
2020-04-02 14:44:11 +0100

    Create server worker class

---
+++ src/ServerWorker.java
@@ -0,0 +1,54 @@
+/**
+ * Server worker class
+ * Author: Anton Samoilov <2459087s@student.gla.ac.uk>, matric 2459087S
+ * ------------
+ * This class does the server work to communicate with every client
+ */
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ServerWorker implements Runnable {
+    private Socket socket = null;
+    private ObjectInputStream inputStream = null;
+    private ObjectOutputStream outputStream = null;
+
+    /**
+     * Constructor
+     */
+    public ServerWorker(Socket socket) {
+        this.socket = socket;
+
+        // Create input and output streams
+        try {
+            outputStream = new ObjectOutputStream(this.socket.getOutputStream());
+            inputStream = new ObjectInputStream(this.socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Run method for the thread
+     */
+    public void run() {
+        try {
+            // TODO: Use event manager for handling input streams
+            // Get an object from a client via input stream
+            Object data = (Object) inputStream.readObject();
+
+            while (data != null) {
+                System.out.println(data);
+            }
+
+            inputStream.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Client disconnected");
+            e.printStackTrace();
+        }
+    }
+}
